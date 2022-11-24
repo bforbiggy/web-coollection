247ea1a7293e6ba3ec3fe14ed88c2a8f5f351754
bforbiggy <AndyHeCS@protonmail.com>
2022-11-24 09:40:17 -0500

    Added basic routing
    Set default route contents
    Added template blobliquidfollow route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BlobliquidfollowComponent } from "./blobliquidfollow/blobliquidfollow.component";
+import { LandingComponent } from "./landing/landing.component";
+
+const routes: Routes = [
+	{ path: "", component: LandingComponent },
+	{ path: "blobliquidfollow", component: BlobliquidfollowComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, BlobliquidfollowComponent, LandingComponent],
+	imports: [BrowserModule, RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/blobliquidfollow/blobliquidfollow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blobliquidfollow',
+  templateUrl: './blobliquidfollow.component.html',
+  styleUrls: ['./blobliquidfollow.component.scss']
+})
+export class BlobliquidfollowComponent {
+
+}

5bfe35167848d601141afcaee715084e1053abd8
bforbiggy <AndyHeCS@protonmail.com>
2022-11-26 20:23:10 -0500

    scss??

--- src/Landing.svelte
+++ src/Landing.svelte
@@ -1,4 +1,18 @@
 
-<p>
-	Hi I'm landing
-</p>
+<div class="outer">
+	I'm outside
+
+	<p class="inner">
+		I'm inside
+	</p>
+</div>
+
+<style lang="scss">
+	.outer{
+		color: red;
+
+		.inner{
+			color: aqua;
+		}
+	}
+</style>

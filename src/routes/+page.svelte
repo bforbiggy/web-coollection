36f9d7bde014bbe7b3c88b631d3d4d962464f398
bforbiggy <AndyHeCS@protonmail.com>
2022-11-26 20:36:58 -0500

    uh fuck

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Table of Contents</h1>
+<a href="/snowflakes">Landin</a>
---
+++ src/routes/snowflakes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="outer">
+	Snowflakes
+	<div class="inner">
+		Snowflakes
+	</div>
+</div>
+
+<style lang="scss">
+	.outer{
+		color: aqua;
+
+		.inner{
+			color: red;
+		}
+	}
+</style>

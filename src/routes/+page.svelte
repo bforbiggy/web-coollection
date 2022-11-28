daf3de18e181296c0259a00821585aa4db00bdcf
bforbiggy <AndyHeCS@protonmail.com>
2022-11-28 09:33:08 -0500

    Basic TOC styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,30 @@
-<h1>Table of Contents</h1>
-<a href="/snowflakes">Snowflakes</a>
-<a href="/liquidfollow">Following Liquid</a>
-<a href="/carvendepict">Carve n' Depict</a>
+<div class="toc">
+	<div class="title">
+		<p>Table of Contents</p>
+	</div>
+	<div class="contents">
+		<a href="/snowflakes">Snowflakes</a>
+		<a href="/liquidfollow">Following Liquid</a>
+		<a href="/carvendepict">Carve n' Depict</a>
+	</div>
+</div>
 
 <style lang="scss">
+	.toc {
+		width: 100vw;
+		height: 100%;
+
+		.title{
+			padding: 0;
+			p{
+				margin: 0;
+				font-size: xx-large;
+			}
+		}
+
+		.contents {
+			display: flex;
+			flex-direction: column;
+		}
+	}
 </style>

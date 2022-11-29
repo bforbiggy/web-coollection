79bbd762f575164bc613566d1bda58173b232b73
bforbiggy <AndyHeCS@protonmail.com>
2022-11-28 21:15:28 -0500

    Eyeball parts

--- src/routes/watchful-eye/+page.svelte
+++ src/routes/watchful-eye/+page.svelte
@@ -1,9 +1,16 @@
 <script>
-
+	import eyeBackSVG from './eye-back.svg';
 </script>
 
 <div class="container">
-	Watching eye
+	<div class="eye">
+		<img src={eyeBackSVG} alt="" />
+
+		<div class="ball">
+			<div class="iris" />
+			<div class="pupil" />
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
@@ -11,8 +18,40 @@
 	$black: #333;
 	$white: azure;
 
-	.container{
+	.container {
 		width: 100vw;
 		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.eye {
+		display: flex;
+		align-self: center;
+		justify-self: center;
+
+		img {
+			position: relative;
+		}
+
+		.ball {
+			position: relative;
+
+
+			.iris {
+				height: 2.5rem;
+				width: 2.5rem;
+				background-color: teal;
+				border-radius: 50%;
+			}
+
+			.pupil {
+				height: 1rem;
+				width: 1rem;
+				background-color: aquamarine;
+				border-radius: 50%;
+			}
+		}
 	}
 </style>

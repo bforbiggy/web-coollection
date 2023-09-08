ced30885ae9c6a69fc15f026f667e6941f9f393a
bforbiggy <AndyHeCS@protonmail.com>
2023-09-08 06:01:49 -0400

    Perfect cropping + Placed together

--- src/routes/image-puzzle/Cropped.svelte
+++ src/routes/image-puzzle/Cropped.svelte
@@ -7,16 +7,19 @@
 	let y = parentData.y;
 	let cropWidth = parentData.width / parentData.xMax;
 	let cropHeight = parentData.height / parentData.yMax;
-	let xOffset = x * cropWidth;
-	let yOffset = y * cropHeight;
+
+	// Calculate cutoffs
+	let yLowerCutoff = Math.max(0, y - 1) * cropHeight;
+	let yUpperCutoff = (parentData.yMax - y) * cropHeight;
+	let xLeftCutoff = Math.max(0, x - 1) * cropWidth;
+	let xRightCutoff = (parentData.xMax - x) * cropWidth;
 
 	// Convert style data to css style text
 	let style = '';
 	style += `grid-column-start: ${x};`;
 	style += `grid-row-start: ${y};`;
-	style += `clip-path: inset(${yOffset}px ${xOffset}px ${yOffset + cropHeight}px ${
-		xOffset + cropWidth
-	}px);`;
+	style += `clip-path: inset(${yLowerCutoff}px ${xRightCutoff}px ${yUpperCutoff}px ${xLeftCutoff}px);`;
+	style += `margin: -${parentData.height - cropHeight}px -${parentData.width - cropWidth}px`;
 </script>
 
 <img {style} src={image} alt="" />

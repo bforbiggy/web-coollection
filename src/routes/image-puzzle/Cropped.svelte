4ee942a7e069ab27237af38dc6f0749cadb78bf8
bforbiggy <AndyHeCS@protonmail.com>
2023-09-08 05:51:32 -0400

    broken insets? Fix now

--- src/routes/image-puzzle/Cropped.svelte
+++ src/routes/image-puzzle/Cropped.svelte
@@ -5,14 +5,18 @@
 	// Calculate style data for the image
 	let x = parentData.x;
 	let y = parentData.y;
-	let cropWidth = 1 / parentData.xMax;
-	let cropHeight = 1 / parentData.yMax;
+	let cropWidth = parentData.width / parentData.xMax;
+	let cropHeight = parentData.height / parentData.yMax;
+	let xOffset = x * cropWidth;
+	let yOffset = y * cropHeight;
 
 	// Convert style data to css style text
 	let style = '';
 	style += `grid-column-start: ${x};`;
 	style += `grid-row-start: ${y};`;
-	style += `clip-path: inset(30px 30px 30px 30px);`;
+	style += `clip-path: inset(${yOffset}px ${xOffset}px ${yOffset + cropHeight}px ${
+		xOffset + cropWidth
+	}px);`;
 </script>
 
 <img {style} src={image} alt="" />
